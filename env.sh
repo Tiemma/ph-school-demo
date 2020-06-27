c4023ee4e78b6c34b7524d503951bf90b7f147a6
Bakare Emmanuel <bakare@deimos.co.za>
2020-06-27 10:38:33 +0100

    initial commit

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CONTAINER=$(basename $PWD)
+echo "Docker Container Name: $CONTAINER" 
+
+CONTAINER_EXISTS=$(docker ps -a | grep $CONTAINER)
+if [[ -z $CONTAINER_EXISTS ]]
+then
+  echo "Creating new dev environment container: $CONTAINER"
+  docker run --name="$CONTAINER" -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/datascience-notebook
+else
+  echo "Container exists: $CONTAINER_EXISTS"
+  docker start $CONTAINER
+fi
